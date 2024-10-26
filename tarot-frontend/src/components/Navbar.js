9635fbec989fcecc7fc752153f0539821d258c2d
OldEphraim <a8garber@yahoo.com>
2024-10-26 17:40:27 -0400

    centered dropdowns with useEffect

--- tarot-frontend/src/components/Navbar.js
+++ tarot-frontend/src/components/Navbar.js
@@ -7,7 +7,9 @@ const Navbar = () => {
     tarot: false,
     account: false,
   });
+  const [dropdownStyles, setDropdownStyles] = useState({}); 
 
+  const dropdownRefs = useRef({ tarot: { toggle: null, menuItem: null }, account: { toggle: null, menuItem: null } });
   const navbarRef = useRef(null);
 
   const toggleDropdown = (menu) => {
@@ -41,18 +43,32 @@ const Navbar = () => {
     };
   }, []);
 
+  useEffect(() => {
+    const newStyles = {};
+    for (const key in dropdownRefs.current) {
+      const ref = dropdownRefs.current[key];
+      if (ref.toggle && ref.menuItem) {
+        const toggleWidth = ref.toggle.offsetWidth;
+        const menuItemWidth = ref.menuItem.offsetWidth;
+        const offset = (menuItemWidth - toggleWidth) / 2;
+        newStyles[key] = { transform: `translateX(-${offset}px)` };
+      }
+    }
+    setDropdownStyles(newStyles);
+  }, [showDropdown]);
+
   return (
     <nav className="navbar" ref={navbarRef}>
       <ul className="nav-links">
         <li><div className="home-link" onClick={() => closeDropdowns()}><Link to="/">Home</Link></div></li>
 
         <li>
-          <div className="dropdown-toggle" onClick={() => toggleDropdown('tarot')}>
+          <div className="dropdown-toggle" ref={(el) => (dropdownRefs.current.tarot.toggle = el)} onClick={() => toggleDropdown('tarot')}>
             About Tarot
           </div>
           {showDropdown.tarot && (
-            <ul className="dropdown-menu">
-              <li><Link to="/tarot/about">Art of Tarot</Link></li>
+            <ul className="dropdown-menu" style={dropdownStyles.tarot}>
+              <li ref={(el) => (dropdownRefs.current.tarot.menuItem = el)}><Link to="/tarot/about">Art of Tarot</Link></li>
               <li><Link to="/tarot/spreads">Tarot Spreads</Link></li>
               <li><Link to="/tarot/cards">Tarot Cards</Link></li>
             </ul>
@@ -60,12 +76,12 @@ const Navbar = () => {
         </li>
 
         <li>
-          <div className="dropdown-toggle" onClick={() => toggleDropdown('account')}>
+          <div className="dropdown-toggle" ref={(el) => (dropdownRefs.current.account.toggle = el)} onClick={() => toggleDropdown('account')}>
             My Account
           </div>
           {showDropdown.account && (
             <ul className="dropdown-menu">
-              <li><Link to="/login">Login</Link></li>
+              <li ref={(el) => (dropdownRefs.current.account.menuItem = el)}><Link to="/login">Login</Link></li>
               <li><Link to="/signup">Sign Up</Link></li>
             </ul>
           )}
