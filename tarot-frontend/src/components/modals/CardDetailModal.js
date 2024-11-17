3a6bec4dfcc5612440364515238375534a23fbf7
OldEphraim <a8garber@yahoo.com>
2024-11-16 23:00:56 -0500

    more image saving logic

--- tarot-frontend/src/components/modals/CardDetailModal.js
+++ tarot-frontend/src/components/modals/CardDetailModal.js
@@ -1,11 +1,45 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { useAuth } from "../../context/AuthContext";
 import { useModal } from "../../context/ModalContext";
 import { useTarot } from "../../context/TarotContext";
 import "../../components/Modal.css";
+import { getSavedImage, handleSaveImage } from "../../services/profileService";
 
 const CardDetailModal = ({ setFadeOut }) => {
+  const [hasSaved, setHasSaved] = useState(false);
+  const [isFavorite, setIsFavored] = useState(false);
+
+  const hasFetchedFavorites = useRef(false);
+
+  const { user } = useAuth();
   const { modalData } = useModal();
   const { selectedSpread, workflow } = useTarot();
+
+  useEffect(() => {
+    const fetchFavorites = async () => {
+      try {
+        const favoriteData = await getSavedImage(user);
+        console.log(favoriteData);
+
+        const isImageAlreadyFavorite = favoriteData.some(
+          (favorite) => favorite.ImageUrl === modalData.imageUrl
+        );
+
+        if (isImageAlreadyFavorite) {
+          setIsFavored(true);
+        } else {
+          setIsFavored(false);
+        }
+      } catch (error) {
+        console.error("Failed to fetch favorites:", error);
+      }
+    };
+
+    if (user && !hasFetchedFavorites.current) {
+      hasFetchedFavorites.current = true;
+      fetchFavorites();
+    }
+  });
 
   const handleScrollToExplanation = () => {
     const targetElement = document.getElementById(
@@ -20,6 +54,20 @@ const CardDetailModal = ({ setFadeOut }) => {
       window.scrollTo({ top: yPosition, behavior: "smooth" });
     }
     setFadeOut(true);
+  };
+
+  const handleSaveClick = async () => {
+    try {
+      await handleSaveImage(
+        user,
+        modalData.imageUrl,
+        modalData.card.details.name,
+        modalData.theme
+      );
+      setHasSaved(true);
+    } catch (error) {
+      console.error("Error saving profile picture to saved images", error);
+    }
   };
 
   return (
@@ -59,6 +107,10 @@ const CardDetailModal = ({ setFadeOut }) => {
         {selectedSpread !== "One" && workflow !== "fortuneteller"
           ? ` in the ${modalData.positionMeaning.text} position.`
           : "."}
+        {hasSaved && (
+          <strong>{" This image has been saved to your favorites."}</strong>
+        )}
+        {isFavorite && "This image has already been saved to your favorites."}
       </p>
       <div>
         <button
@@ -67,12 +119,11 @@ const CardDetailModal = ({ setFadeOut }) => {
         >
           See Explanation
         </button>
-        <button
-          className="spooky-button"
-          onClick={() => alert("Saved to Favorites!")}
-        >
-          Save to Favorites
-        </button>
+        {!isFavorite && (
+          <button className="spooky-button" onClick={() => handleSaveClick()}>
+            Save to Favorites
+          </button>
+        )}
       </div>
     </>
   );
